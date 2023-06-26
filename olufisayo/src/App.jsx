809ee53847e4a36d1a6e96263f4b0155ec2f5aed
Olufisayo Israel Tomide <115098511+KodarkBlack@users.noreply.github.com>
2023-06-26 03:51:46 +0100

    portfolio

---
+++ olufisayo/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ olufisayo/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div id="main">
+      <img src="https://unsplash.com/photos/odKeTFsBDgE" alt="..." />
+
+      <div>
+        <h1>I'm Israel Olufisayo</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
