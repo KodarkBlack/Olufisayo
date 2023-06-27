e0245de387f4c5c6451176ca8f18ebd4ea21f5c3
Olufisayo Israel Tomide <115098511+KodarkBlack@users.noreply.github.com>
2023-06-27 04:41:34 +0100

    push
    port

--- olufisayo/src/components/Home.jsx
+++ olufisayo/src/components/Home.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
+import {TypeAnimation} from 'react-type-animation'
 
 const Home = () => {
   return (
     <div id="main">
-      <img src="https://unsplash.com/photos/odKeTFsBDgE" alt="..." />
+      <img  src="https://unsplash.com/photos/XJXWbfSo2f0" alt="" />
 
       <div>
-        <h1>I'm Israel Olufisayo</h1>
+        <h1 className="text-3xl  text-startup-blue">I'm Israel Olufisayo</h1>
+        <h2>
+        <TypeAnimation
+          sequence={[
+            // Same substring at the start will only be typed out once, initially
+            'I am a software Enthusiast  ',
+            1000, // wait 1s before replacing "Mice" with "Hamsters"
+            'I am a software Engineer  ',
+            1000,
+            'I am a software Programmer',
+            1000,
+            'I am a software Badass',
+            1000
+          ]}
+          wrapper="span"
+          speed={50}
+          style={{ fontSize: '2em', display: 'inline-block' }}
+          repeat={Infinity}
+        />
+        </h2>
+        <div>
+          <FaTwitter/>
+          <FaFacebook/>
+          <FaInstagram/>
+        </div>
       </div>
     </div>
   )
--- olufisayo/tailwind.config.js
+++ olufisayo/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+
+      
+    },
   },
   plugins: [],
 }
+
